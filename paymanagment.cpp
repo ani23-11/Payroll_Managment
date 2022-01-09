8d0d2049b9ea0e7c48e398c36728aae9b6ff066c
ani2311 <aniket21mishra@gmail.com>
2022-01-09 13:11:06 +0530

    commit1

---
+++ paymanagment.cpp
@@ -0,0 +1,147 @@
+#include<iostream>
+#include<conio.h>
+#include<stdio.h>
+#include<fstream>
+#include<string.h>
+using namespace std;
+class Employee
+ {
+  public:
+        string Name[20], Desg[20], id[20];
+        
+        float workday, halfday, uninfoleave, payperday, bonus, number;
+        fstream mf;
+
+    void E_details(int j)
+        { cout<<endl;
+        string ddt[60]; 
+	    fstream mf; 
+	    mf.open("codedata.csv", ios::in);
+        int x=j,y=0;
+	    while(x--)
+        { 
+		getline(mf,ddt[y],',');
+                cout<<endl<<"Enter The Details Of Employee "<<ddt[y]<<" : "<<endl;
+            getdata();
+                cout<<"Name -> "<<ddt[y]<<endl;
+        getline(mf,ddt[y+1],',');
+	            cout<<"Designation -> "<<ddt[y+1]<<endl;
+        getline(mf,ddt[y+2],',');
+	            cout<<"ID Number -> "<<ddt[y+2]<<endl;
+        
+        P_details(ddt[y]);       
+		y++;
+	    } 
+        mf.close();
+        }   
+	  
+	void P_details(string Name)
+	   {
+	   float x, y, z,m=0;
+	   
+	   cout<<"Pay Per Day -> Rs."<<payperday<<endl;
+	   cout<<"Days Of Working -> "<<workday<<endl;
+	      x=payperday*workday;
+	   cout<<"Gross Payment -> Rs."<<x<<endl;
+	   cout<<"Half Days -> "<<halfday<<endl;
+	   cout<<"Un Informed Leaves -> "<<uninfoleave<<endl;
+	   cout<<"%Medical Tax -> 2%"<<endl;
+	   cout<<"%Social Security Tax -> 1.5%"<<endl;
+	     if(payperday<1200)
+	      y=400*halfday;
+	     else
+	      y=600*halfday;
+	   cout<<"Half Day Deductions -> Rs."<<y<<endl;
+	      z=300*uninfoleave;
+	   cout<<"Un Informed Leave Deduction -> Rs."<<z<<endl;
+       cout<<"Bonus -> Rs."<<bonus<<endl;
+	   cout<<"Net Deductions -> Rs."<<(x*0.02)+(x*0.015)+y+z<<endl;
+	   cout<<"Net Payment -> Rs."<<x-((x*0.02)+(x*0.015)+y+z)+bonus<<"\n\n\n\n";
+
+       cout<<"Do you want to print details to a file?(1=Yes & 0=No)";
+        cin>>m;
+            if(m==1)
+                {
+                    ofstream myfile;
+                    myfile.open ("employee.txt",std::ios_base::app);
+                    myfile << "---------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------\n";
+                    myfile << "										DUNDER MIFFLIN PAPER COMPANY\n";
+                    myfile << "										   PAYSLIP December 2021\n";
+                    myfile << "---------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------\n";
+                    myfile << "NAME : "<<Name<<endl;
+                    myfile << "Gross Salary : Rs. "<<x<<endl;
+                    myfile << "Total Leave : "<<31-workday-halfday<<endl;
+                    myfile << "Net Deductions : Rs. "<<(x*0.02)+(x*0.015)+y+z<<endl;
+                    myfile << "Bonus : Rs. "<<bonus<<endl;
+                    myfile << "Net Payment : Rs. "<<x-((x*0.02)+(x*0.015)+y+z)+bonus<<endl;
+                    myfile << "---------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------\n";
+                    myfile.close();
+                }
+            else
+                m=1;
+	   }
+
+    void getdata()
+    {
+            cout<<"Pay Per Day : ";
+	            cin>>payperday;
+	            cout<<"Enter Total Number Of Working Days : ";
+	            cin>>workday;
+	            cout<<"Halfdays : ";
+	             cin>>halfday;
+	            cout<<"Number Of Un Informed Leaves : ";
+	            cin>>uninfoleave;
+                cout<<"Bonus : ";
+	            cin>>bonus;
+	            cout<<endl<<endl;
+    }
+ };
+
+ class emp : public Employee
+{ public:
+
+void E_details()
+	  { string Name, Desg, id;
+        cout<<endl<<endl;
+	    cin.ignore();
+	    cout<<"Enter Name : ";
+	     getline(cin,Name);
+	    cout<<"Enter Designation : ";
+	     getline(cin,Desg);
+	    cout<<"Enter Employee's ID Number : ";
+	     getline(cin,id);
+	    getdata();
+        cout<<"Name -> "<<Name<<endl;
+	   cout<<"Designation -> "<<Desg<<endl;
+	   cout<<"ID Number -> "<<id<<endl;
+        P_details(Name);
+	  }
+
+};
+
+
+main()
+ {
+ int i,j,k;
+
+ cout<<"\n\t\t\t\t\t\t\t\t\t\tDunder Mifflin Paper Company\n\t\t\t\t\t\t\t\t\t\t  PAYROLL of December 2021\n\n\n";
+ cout<<"Enter The Number Of Emplyoees : ";
+ cin>>j;
+ Employee Ei;
+ emp ei;
+ cout<<endl<<"If you want to use data from file enter 1\nIf you want to eneter a new Employee data enter 0"<<endl;
+ cin>>k;
+ if(k==1)
+    Ei.E_details(j);
+
+ else if(k==0)
+    {
+    for(i=1;i<=j;i++)  
+        ei.E_details();
+    }
+ else
+    cout<<"Error";
+  
+ getch();
+ return 0;
+ }
